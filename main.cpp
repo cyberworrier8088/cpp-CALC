172803f2a68d3db227e59c7acfa9dee711639ef3
cyberworrier8088 <cyberworrier39@gmail.com>
2026-08-19 14:23:22 +0530

    feat: frnt end fully added like case and ec. next need functin for plus and minus and etc

--- main.cpp
+++ main.cpp
@@ -6,5 +6,29 @@ int main() {
     char opration;
     
     std::cout << "Enter Your First Number: ";
+    std::cin >> first_Number;
+
+    std::cout << "Enter Your Operation (+,-,×,÷): ";
+    std::cin >> opration;
+
+    std::cout << "Enter Your Second Number: ";
+    std::cin >> last_Number;
+
+
+    switch (opration) {
+        case '+':
+        plus(first_Number, last_Number);
+        break;
+        case '-':
+        minus(first_Number, last_Number);
+        break;
+        case '×':
+        multiplication (first_Number, last_Number);
+        break;
+        case '÷':
+        divide(first_Number, last_Number);
+        break;
+    }
+
 
 }
